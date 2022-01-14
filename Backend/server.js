7621035e6bce65305ebecc8b08b7b462215b1a94
Maaz Butt <maazbutt205@gmail.com>
2022-01-14 21:20:39 +0500

    Prepare for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -22,34 +22,49 @@ import uploadRoutes from "./routes/uploadRoutes.js"
 
 import morgan from "morgan"
 
-const app=express()
 
-app.use(express.json())
+config()
+connectDB()
+
+const app=express()
 
 if(process.env.NODE_ENV==="development"){
     
     app.use(morgan("dev"))
 }
 
+app.use(express.json())
+
 app.use("/api/products",productsRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders",orderRoutes)
 app.use("/api/upload",uploadRoutes)
 
-
-const __dirname = path.resolve()
-app.use("/uploads",express.static(path.join(__dirname,"/uploads")))
-
 app.get("/api/config/paypal",(req,res)=>
 res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
+const __dirname = path.resolve()
+app.use("/uploads",express.static(path.join(__dirname,"/uploads")))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontEnd/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontEnd', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
+
+
+  
+
 app.use(notFound)
 app.use(errorHandler)
 
-config()
-
-connectDB()
 
 const PORT=process.env.PORT||5000;
        
